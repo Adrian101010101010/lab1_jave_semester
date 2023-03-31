eccfe7f33368253532cf0c80c4392646fc448fe5
Adrian101010101010 <119243847+Adrian101010101010@users.noreply.github.com>
2023-03-31 19:58:33 +0300

    lab2

---
+++ src/ua/lviv/iot/algo/part1/lab1/AerialVehicle.java
@@ -0,0 +1,15 @@
+package ua.lviv.iot.algo.part1.lab1;
+
+import lombok.*;
+@ToString
+
+public abstract class AerialVehicle {
+    public String manufacturer;
+    public int maxSpeed;
+    private int FlyingDistance;
+    private int DeliveryWeight;
+
+
+    public int getMaxFlyingDistance(){return FlyingDistance;}
+    public int getMaxDeliveryWeight(){return DeliveryWeight; }
+}
---
+++ src/ua/lviv/iot/algo/part1/lab1/Drone.java
@@ -0,0 +1,33 @@
+package ua.lviv.iot.algo.part1.lab1;
+
+import lombok.*;
+@ToString
+
+public  class Drone extends AerialVehicle{
+    private int batteryCapacity;
+    private int chargeConsumptionPerMinuteOfFlight;
+    private int speed;
+
+    public Drone(String manufacturer,int chargeConsumptionPerMinuteOfFlight){
+        this.manufacturer = manufacturer;
+        this.chargeConsumptionPerMinuteOfFlight = chargeConsumptionPerMinuteOfFlight;
+    }
+    public Drone(){}
+
+    private final static Drone defaultDrone = new Drone();
+
+     public  Drone fast(int acceleration){
+        int newSpeed;
+        newSpeed = speed + acceleration;
+        if (newSpeed > maxSpeed){
+            newSpeed = maxSpeed;
+        }
+        if (batteryCapacity == 50){
+            maxSpeed = maxSpeed/2;
+        }
+        if (batteryCapacity ==0){
+            maxSpeed =0;
+        }
+        return defaultDrone;
+    }
+}
